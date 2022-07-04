7ac95361113b482ac2c903ab499440c289900b2e
Alimoff <sobiriddin@gmail.com>
2022-07-04 08:55:13 +0500

    add:videos model

--- videos/models.py
+++ videos/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Videos(models.Model):
+    name = models.CharField("Name:", max_length=250)
+    link = models.URLField("Link:", max_length=300)
+    text = models.TextField("Description:")
+
+    # more info
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Video"
+        verbose_name_plural = "Videos"
